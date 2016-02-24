72df0b707abb60d9171f9fc5b060d10f43de0f97
Neville Dsouza <nevilledsouza28@gmail.com>
2016-02-25 01:45:28 +0530

    created a function redirectTo()

--- build/js/manage-lot.js
+++ build/js/manage-lot.js
@@ -1,160 +1,288 @@
-$.aaacplApp.manageDept.getLayout = function (){
+$.aaacplApp.manageLot.getLayout = function (){
 
 	/***
-	** COMPLETE DEPARTMENT PAGE LAYOUT
+	** COMPLETE LOT PAGE LAYOUT
 	**/
-	var tmpl = '<div id="departments" class="box box-solid manage">'+
+	var tmpl = '<div id="form-success">'+
+               '<div class="alert alert-success">'+
+               '<strong>Lot has been created/updated successfully! </strong>'+
+               '</div>'+
+               '</div>'+
+			   '<div id="form-failure">'+
+              '<div class="alert alert-danger">'+
+              '<strong>Error !</strong> <span class="message-text"></span>'+
+              '</div>'+
+              '</div>'+
+	'<div class="box box-solid manage">'+
              '<div class="box-header">'+
-               '<h3 class="box-title">Departments</h3>'+
+               '<h3 class="box-title">Lots</h3>'+
 			   '<div class="box-tools pull-right">'+
-			   '<button class="btn bg-orange" data-toggle="modal" data-target="#add-dept-form">New Department</button>'+
+			   '<button class="btn bg-orange" data-toggle="modal" data-target="#add-lot-form">Add New Lot</button>'+
 			   '</div>'+
             '</div>'+
-            '<div class="box-body" id="dept-rows-cont">'+
-			'</div><!-- /.box-body -->'+'<div class="overlay" style="display:none"><i class="fa fa-refresh fa-spin"></i></div>'+
-         '</div>'+
+            '<div class="box-body" id="lot-rows-cont">'+
+			'</div><!-- /.box-body -->'+
+			'<div class="overlay" style="display:none"><i class="fa fa-refresh fa-spin"></i></div>'+
+         ' </div>'+
 
-		 //Modal for adding new department
-		 '<div class="modal fade" tabindex="-1" role="dialog" id="add-dept-form" aria-labelledby="model-heading">'+
+		 //Modal for adding new lots
+		 '<div class="modal fade" tabindex="-1" role="dialog" id="add-lot-form" aria-labelledby="model-heading">'+
           '<div class="modal-dialog" role="document">'+
-           ' <div class="modal-content">'+
+           ' <div class="modal-content box">'+
               '<div class="modal-header">'+
                ' <button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
                '   <span aria-hidden="true">×</span></button>'+
-               ' <h4 class="modal-title" id="model-heading">New Department</h4>'+
+               ' <h4 class="modal-title" id="model-heading">Add New Lot</h4>'+
               '</div>'+
-			  '<form class="form" role="form">'+
+               '<div id="createLotsFormSection">'+
+			  '<form id="createLotsForm" class="form" role="form">'+
               '<div class="modal-body">'+
 			 '<div class="form-group">'+
-			  ' <label for="deptInputName">Department Name</label>'+
-			   ' <input type="text" class="form-control" name="name" id="deptInputName" required>'+
+			  ' <label for="lotInputName">Lot Name</label>'+
+			   ' <input type="text" class="form-control" id="lotInputName" name="name" required>'+
 			 '</div>'+
-			 '<div class="form-group">'+
-			 ' <label for="deptInputLogoFile">Department Logo</label>'+
-			   ' <input type="file" class="form-control" name="logoPath" id="deptInputLogoFile">'+
-			 '</div>'+
-              '</div>'+
+			 '<!-- startBid -->'+
+                 '<div class="form-group">'+
+                 '<label>Start Bid</label>'+
+                 '<input type="text" class="form-control" id="lotStartBid" name="startBid" required>'+
+                 '</div>'+
+                 '<!-- Difference Factor-->'+
+                  '<div class="form-group">'+
+                  '<label>Difference Factor</label>'+
+                  '<input type="text" class="form-control" id="lotdifferenceFactor" name="differenceFactor" required>'+
+                  '</div>'+
+			 '<!-- Date and time range -->'+
+                  '<div class="form-group">'+
+                   ' <label>Lot start and end date:</label>'+
+                   ' <div class="input-group">'+
+                    '  <div class="input-group-addon">'+
+                    '    <i class="fa fa-clock-o"></i>'+
+                    '  </div>'+
+                    '  <input type="text" class="form-control pull-right" id="lotDateRange" required>'+
+                    '</div><!-- /.input group -->'+
+                  '</div><!-- /.form group -->'+
+                  '<!-- Description -->'+
+                                    '<div class="form-group">'+
+                                    '<label>Description</label>'+
+                                    '<textarea class="form-control" id="lotDescription" name="description" required></textarea>'+
+                                    '</div>'+
+                                      '</div>'+
+                                     '<!-- /.modal-body -->'+
               '<div class="modal-footer">'+
               '  <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>'+
               '  <button type="submit" class="btn btn-primary">Save changes</button>'+
               '</div>'+
 			  '</form>'+
+               '</div>'+'<div class="overlay" style="display:none"><i class="fa fa-refresh fa-spin"></i></div>'+
             '</div>'+
             '<!-- /.modal-content -->'+
           '</div>'+
-          '<!-- /.modal-dialog -->'+'<div class="overlay" style="display:none"><i class="fa fa-refresh fa-spin"></i></div>'+
+          '<!-- /.modal-dialog -->'+
         '</div>';
 
 	return tmpl;
 };
 
-$.aaacplApp.manageDept.executeScript = function(){
+$.aaacplApp.manageLot.executeScript = function(){
 	var _this = this;
 
-	_this.loadDeptRows();
+	// be default hiding the success and error alert messages
+		$('#form-success').hide();
+		$('#form-failure').hide();
 
-	var addNewDeptForm = $("#add-dept-form form");
-	addNewDeptForm.submit(function(event){
-		event.preventDefault(); // Prevent the form from submitting via the browser
-		var formData = addNewDeptForm.serializeArray(); // JSON data of values entered in form
-		var payload = {};
-		$.each(formData, function (key, item) {
-			payload[item.name] = item.value;
-		});
-		$(".overlay").show();
-		$.aaacplApp.ajaxCall("POST","department/create",function success(response){
-			$(".overlay").hide();
-			if(response.successMessage && response.successMessage == "SUCCESS"){
-				$('#add-dept-form').modal('hide');
-				_this.loadDeptRows();
-			}
-		}, function error(msg){
-			$(".overlay").hide();
-		}, JSON.stringify(payload));
+
+	var createLotsForm = $('#createLotsForm');
+
+	$('#add-lot-form').on('shown.bs.modal', function () {
+	  createLotsForm[0].reset();
 	});
+	// on submit function of form is called to perform client side validation
+	createLotsForm.submit(function(event){
+		event.preventDefault(); // Prevent the form from submitting via the browser
+		var dateRangeValue = $('#lotDateRange').val(); // getting the entire dateRange value
+		var formData = createLotsForm.serializeArray(); // JSON data of values entered in form
+		var lotsPost = {};
+			 $.each(formData, function (key, item) {
+							 lotsPost[item.name] = item.value;
+						 });
+			 lotsPost["auctionId"] = $.aaacplApp.queryParams('auctionTypeId');
+			 lotsPost["startDate"] = typeof dateRangeValue === "string" ? dateRangeValue.substr(0, 19) : "" ;
+			 lotsPost["endDate"] =  typeof dateRangeValue === "string" ? dateRangeValue.substr(21, 20) : "" ;
+			 lotsPost["createdBy"] = $.aaacplApp.getLoggedInUserId();
+		$(".overlay").show();
+		$.aaacplApp.ajaxCall("POST", 'lots/create', function success(response){
+			$(".overlay").hide();
+			$("#add-lot-form").modal('hide');
+			//if(response.successMessage && response.successMessage != ""){
+				$('#form-success').show();
+				_this.loadLotRows();
+			/*} else {
+				$('#form-failure').show();
+				$('#form-failure .message-text').html('Unable to create lot. Please try again.');
+				}*/
+			}, function error(msg){
+			$(".overlay").hide();
+			$("#add-lot-form").modal('hide');
+			$('#form-failure').show();
+			$('#form-failure .message-text').html('Unable to create auction. Please try again later.');
+		},
+		//POST PAYLOAD
+		JSON.stringify(lotsPost));
+	});
+
+
+
+	$('#lotDateRange').daterangepicker({timePicker: true, timePickerIncrement: 1, format: 'YYYY-MM-DD hh:mm:ss'});
+
+	_this.loadLotRows();
+
 
 };
 
-$.aaacplApp.manageDept.loadDeptRows = function (){
-	$(".overlay").show();
-	$.aaacplApp.ajaxCall("GET","department/list",function success(response){
-		$(".overlay").hide();
-		$("#dept-rows-cont").html('');
-		var deptList = response.departmentResponseList;
-		$.each(deptList, function(key , value){
 
-			var deptRow = '<div class="box box-default box-solid collapsed-box dept-row" id="dr-'+value.id+'">'+
-			' <div class="box-header with-border">'+
-			'  <h3 class="box-title">'+value.name+'</h3>'+
-			 ' <div class="box-tools pull-right">'+
-			  '  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i> EDIT</button>'+
-			  '  <a href="#/manage/auctions?deptid='+value.id+'" class="btn btn-box-tool"><i class="fa fa-hdd-o"></i> MANAGE AUCTIONS</a>'+
-			  '</div>'+
-			'</div>'+
-			'<form id="editDeptForm'+value.id+'" class="form" role="form">'+
+$.aaacplApp.manageLot.loadLotRows = function(){
+	if($.aaacplApp.queryParams('auctionTypeId') != ""){
+		$(".overlay").show();
+		//$.aaacplApp.ajaxCall("GET","lots/list/"+$.aaacplApp.queryParams('auctionTypeId'),function success(response){
+		$.aaacplApp.ajaxCall("GET","lots/list/3",function success(response){
+			$(".overlay").hide();
+			$("#lot-rows-cont").html('');
+			var lotList = response.lotsResponseList || [];
+			$.each(lotList, function(key , value){
+
+				var lotRow = '<div class="box box-default box-solid collapsed-box lot-row" id="ar-'+value.id+'">'+
+				' <div class="box-header with-border">'+
+				'  <h3 class="box-title">'+value.name+'</h3>'+
+				 ' <div class="box-tools pull-right">'+
+				  '  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i> EDIT</button>'+
+				  '  <button type="button" class="btn btn-box-tool" data-toggle="modal" data-target="#manageParticipator-form"><i class="fa fa-hdd-o"></i> MANAGE PARTICIPATORS</button>'+
+				  '</div>'+
+				'</div>'+
+				'<form id="editLotForm'+value.id+'" class="form" role="form">'+
 				'<div class="box-body">'+
-			'<div id="deptEdit-success">'+
-               '<div class="alert alert-success">'+
-               '<strong>Department has been updated successfully! </strong>'+
-               '</div>'+
-               '</div>'+
-              '<div id="deptEdit-failure">'+
-              '<div class="alert alert-danger">'+
-              '<strong>Error !</strong> <span class="message-text"></span>'+
-              '</div>'+
-              '</div>'+
-			 '<div class="form-group">'+
-			  ' <label for="dept'+value.id+'InputName">Department Name</label>'+
-			   ' <input type="text" class="form-control" id="dept'+value.id+'InputName" name="name" value="'+value.name+'" required>'+
-			 '</div>'+
-			 '<div class="form-group">'+
-			 ' <label for="dept'+value.id+'InputLogoFile">Department Logo</label>'+
-			   ' <input type="file" class="form-control" name="logoPath" id="dept'+value.id+'InputLogoFile">'+
-			 '</div>'+
-			'</div>'+
-			'<div class="box-footer">'+
-				'<button type="submit" class="btn bg-orange">UPDATE</button>'+
-                ' <button type="button" class="btn" data-dismiss="modal">Reset</button>'+
-                	'</div>'+
-			'</form>'+
-		'</div>';
+				'<div id="editLotFormSection">'+
+                   '<div id="lotEdit-success">'+
+                   '<div class="alert alert-success">'+
+                   '<strong>Lot has been saved successfully! </strong>'+
+                   '</div>'+
+                   '</div>'+
+                  '<div id="lotEdit-failure">'+
+                  '<div class="alert alert-danger">'+
+                  '<strong>Error !</strong> <span class="message-text"></span>'+
+                  '</div>'+
+                  '</div>'+
+				 '<div class="form-group">'+
+				  ' <label for="lot'+value.id+'InputName">Lot Name</label>'+
+				   ' <input type="text" name="name" class="form-control" id="lot'+value.id+'InputName" value="'+value.name+'">'+
+				 '</div>'+
+				 '<!-- startBid -->'+
+                  '<div class="form-group">'+
+                  '<label>Start Bid</label>'+
+                  '<input type="text" class="form-control" id="lotStartBid" value="'+value.startBid+'" name="startBid" required>'+
+                  '</div>'+
+                  '<!-- Difference Factor-->'+
+                   '<div class="form-group">'+
+                   '<label>Difference Factor</label>'+
+                   '<input type="text" class="form-control" id="lotdifferenceFactor" value="'+value.differenceFactor+'" name="differenceFactor" required>'+
+                   '</div>'+
+				 '<!-- Date and time range -->'+
+                   '<div class="form-group">'+
+                    ' <label>Lot start and end date:</label>'+
+                    ' <div class="input-group">'+
+                     '  <div class="input-group-addon">'+
+                     '    <i class="fa fa-clock-o"></i>'+
+                     '  </div>'+
+                     '  <input type="text" class="form-control pull-right" id="lot'+value.id+'DateRange" value="'+value.startDate+' - '+value.endDate+'">'+
+                     '  </div><!-- /.input group -->'+
+                   '</div><!-- /.form group -->'+
+				 '<!-- Description -->'+
+                     '<div class="form-group">'+
+                     '<label>Description</label>'+
+                     '<textarea class="form-control" name="description" id="lot'+value.id+'InputName" value="'+value.description+'"></textarea>'+
+                     '</div>'+
+				'</div>'+
+				'</div>'+
+				'<div class="box-footer">'+
+					'  <button type="submit" class="btn bg-orange">UPDATE</button>'+
+					'  <button type="button" class="btn">Reset</button>'+
+				'</div>'+
+				'</form>'+
+				 //Modal for managing participator
+                		 '<div class="modal fade" tabindex="-1" role="dialog" id="manageParticipator-form" aria-labelledby="model-heading">'+
+                          '<div class="modal-dialog" role="document">'+
+                           ' <div class="modal-content">'+
+                              '<div class="modal-header">'+
+                               ' <button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+                               '   <span aria-hidden="true">×</span></button>'+
+                               ' <h4 class="modal-title" id="model-heading">Add participator</h4>'+
+                              '</div>'+
+                			  '<form class="participatorForm" role="form">'+
+                              '<div class="modal-body">'+
+                			 '<div class="form-group">'+
+                			  ' <label for="deptInputName">Select Participator</label>'+
+                			  ' <select id="selectParticipator" multiple="multiple" required></select>'+
+                			 '</div>'+
+                              '</div>'+
+                              '<div class="modal-footer">'+
+                              '  <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>'+
+                              '  <button type="submit" class="btn btn-primary">Assign</button>'+
+                              '</div>'+
+                			  '</form>'+
+                            '</div>'+
+                            '<!-- /.modal-content -->'+
+                          '</div>'+
+                          '<!-- /.modal-dialog -->'+'<div class="overlay" style="display:none"><i class="fa fa-refresh fa-spin"></i></div>'+
+                        '</div>';
+			'</div>';
 
-		 $("#dept-rows-cont").append(deptRow);
+			 $("#lot-rows-cont").append(lotRow);
+			 $('#lot'+value.id+'DateRange').daterangepicker({timePicker: true, timePickerIncrement: 1, format: 'YYYY-MM-DD hh:mm:ss'});
 
-		  $('#deptEdit-success').hide();
-          $('#deptEdit-failure').hide();
+			 var dummydata = [{ id: 0, text: 'user1' }, { id: 1, text: 'user2' }, { id: 2, text: 'user3'}];
+			 $("#selectParticipator").select2({
+			    data: dummydata
+			 });
 
-        $('#editDeptForm' + value.id).submit(function(event){
-            var deptID = event.target.id.replace('editDeptForm','');
-            event.preventDefault(); // Prevent the form from submitting via the browser
-            var formData = $('#editDeptForm' + deptID).serializeArray(); // JSON data of values entered in form
-            var deptPost = {};
-                 $.each(formData, function (key, item) {
-                                 deptPost[item.name] = item.value;
-                             });
-                 deptPost["id"] = deptID;
-            $(".overlay").show();
-            $.aaacplApp.ajaxCall("PUT", 'department/update', function success(response){
-                $(".overlay").hide();
-                if(response.successMessage && response.successMessage != ""){
-                    $('#deptEdit-success').show();
-                } else {
-                    $('#deptEdit-failure').show();
-                    $('#deptEdit-failure .message-text').html('Unable to update dept. Please try again.');
-                }
-            }, function error(msg){
-                $(".overlay").hide();
-                $('#deptEdit-failure').show();
-                $('#deptEdit-failure .message-text').html('Unable to update dept. Please try again later.');
-            },
-            //POST PAYLOAD
-            JSON.stringify(deptPost));
-        });
+			 // be default hiding the success and error alert messages
+                $('#lotEdit-success').hide();
+                $('#lotEdit-failure').hide();
 
+				$('#editLotForm' + value.id).submit(function(event){
+					var lotID = event.target.id.replace('editLotForm','');
+					event.preventDefault(); // Prevent the form from submitting via the browser
+					var dateRangeValue = $('#lot'+lotID+'DateRange').val(); // getting the entire dateRange value
+					var formData = $('#editLotForm' + lotID).serializeArray(); // JSON data of values entered in form
+					var lotsPost = {};
+						 $.each(formData, function (key, item) {
+										 lotsPost[item.name] = item.value;
+									 });
+						 lotsPost["id"] = lotID;
+						 lotsPost["startDate"] = typeof dateRangeValue === "string" ? dateRangeValue.substr(0, 19) : "" ;
+						 lotsPost["endDate"] =  typeof dateRangeValue === "string" ? dateRangeValue.substr(21, 20) : "" ;
+						 lotsPost["updatedBy"] = $.aaacplApp.getLoggedInUserId();
+					$(".overlay").show();
+					$.aaacplApp.ajaxCall("PUT", 'lots/update', function success(response){
+						$(".overlay").hide();
+						if(response.successMessage && response.successMessage != ""){
+							$('#lotEdit-success').show();
+						} else {
+							$('#lotEdit-failure').show();
+							$('#lotEdit-failure .message-text').html('Unable to update lot. Please try again.');
+						}
+					}, function error(msg){
+						$(".overlay").hide();
+						$('#lotEdit-failure').show();
+						$('#lotEdit-failure .message-text').html('Unable to update lot. Please try again later.');
+					},
+					//POST PAYLOAD
+					JSON.stringify(lotsPost));
+				});
+
+
+			});
+		}, function error(msg){
+			$(".overlay").hide();
 		});
-	}, function error(msg){
-		$(".overlay").hide();
-	});
-}
-
+	}
+};
      
